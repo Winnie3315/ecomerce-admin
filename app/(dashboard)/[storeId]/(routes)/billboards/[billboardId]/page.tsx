985358799fa7025994dacef2bf69337f13aa97ef
Winnie3315 <winniiemae777@gmail.com>
2024-12-29 23:10:20 +0500

    fixed vercel error try 1

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,29 +1,30 @@
 import prismadb from "@/lib/prismadb";
 import BillboardsForm from "./components/BillboardsForm";
 
-const BillboardPage = async ({
-    params
-}: {
-    params: { billboardId: string }
-}) => {
-    if (!params.billboardId) {
-        return <div>Error: Billboard ID is missing.</div>;
-    }
+interface BillboardPageProps {
+  params: { billboardId: string };
+}
 
-    const billboard = await prismadb.billboard.findUnique({
-        where: {
-            id: params.billboardId,
-        },
-    });
+const BillboardPage = async ({ params }: BillboardPageProps) => {
+  const { billboardId } = params;
 
+  if (!billboardId) {
+    return <div>Error: Billboard ID is missing.</div>;
+  }
 
-    return (
-        <div className="flex-col">
-            <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardsForm initialData={billboard} />
-            </div>
-        </div>
-    );
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: billboardId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardsForm initialData={billboard} />
+      </div>
+    </div>
+  );
 };
 
 export default BillboardPage;
